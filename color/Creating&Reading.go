7ec6b07a8fe9db04d847a4227cbca42fd67db0b5
Sasha239 <alex.bogdanov05@mail.ru>
2024-04-26 00:36:00 +0300

    Implemented the Color.Creating&Reading module except p5.Color class. For now, it is possible to use js.Value instead

---
+++ color/Creating&Reading.go
@@ -0,0 +1,86 @@
+package color
+
+import (
+	"p5js-wasm-go/utils"
+	"syscall/js"
+)
+
+func Alpha(args ...js.Value) (js.Value, error) {
+	res, err := utils.AnyFuncReturnJsValue("alpha", []int{1}, args...)
+	if err != nil {
+		return js.Null(), err
+	}
+	return res, nil
+}
+
+func Blue(args ...js.Value) (js.Value, error) {
+	res, err := utils.AnyFuncReturnJsValue("blue", []int{1}, args...)
+	if err != nil {
+		return js.Null(), err
+	}
+	return res, nil
+}
+
+func Brightness(args ...js.Value) (int, error) {
+	res, err := utils.AnyFuncReturnJsValue("brightness", []int{1}, args...)
+	if err != nil {
+		return -1, err
+	}
+	return res.Int(), nil
+}
+
+func Color(args ...js.Value) (js.Value, error) {
+	res, err := utils.AnyFuncReturnJsValue("color", []int{1, 2, 3, 4}, args...)
+	if err != nil {
+		return js.Null(), err
+	}
+	return res, nil
+}
+
+func Green(args ...js.Value) (js.Value, error) {
+	res, err := utils.AnyFuncReturnJsValue("green", []int{1}, args...)
+	if err != nil {
+		return js.Null(), err
+	}
+	return res, nil
+}
+
+func Hue(args ...js.Value) (js.Value, error) {
+	res, err := utils.AnyFuncReturnJsValue("hue", []int{1}, args...)
+	if err != nil {
+		return js.Null(), err
+	}
+	return res, nil
+}
+
+func LerpColor(args ...js.Value) (js.Value, error) {
+	res, err := utils.AnyFuncReturnJsValue("lerp_color", []int{3}, args...)
+	if err != nil {
+		return js.Null(), err
+	}
+	return res, nil
+}
+
+func Lightness(args ...js.Value) (js.Value, error) {
+	res, err := utils.AnyFuncReturnJsValue("lightness", []int{1}, args...)
+	if err != nil {
+		return js.Null(), err
+	}
+	return res, nil
+}
+
+func Red(args ...js.Value) (js.Value, error) {
+	res, err := utils.AnyFuncReturnJsValue("red", []int{1}, args...)
+	if err != nil {
+		return js.Null(), err
+	}
+	return res, nil
+}
+
+func Saturation(args ...js.Value) (js.Value, error) {
+	res, err := utils.AnyFuncReturnJsValue("saturation", []int{1}, args...)
+	if err != nil {
+		return js.Null(), err
+	}
+	return res, nil
+}
