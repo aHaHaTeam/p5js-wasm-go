5d5b367ffb35f9e0416fbfe14b164e4f74ae5ea1
Sasha239 <alex.bogdanov05@mail.ru>
2024-04-26 01:32:32 +0300

    Finished forgotten rendering module except drawingContext class

--- rendering/rendering.go
+++ rendering/rendering.go
@@ -5,11 +5,34 @@ import (
 	"syscall/js"
 )
 
-func CreateCanvas(width, height int, args ...any) (js.Value, error) {
-	res, err := AnyFuncReturnJsValue("createCanvas", []int{2, 3, 4}, args...)
-	if err != nil {
-		return res, err
-	}
-	return res, nil
+func CreateCanvas(args ...any) (js.Value, error) {
+	return AnyFuncReturnJsValue("createCanvas", []int{2, 3, 4}, args...)
+}
 
+func ResizeCanvas(args ...any) error {
+	return AnyFuncReturnErr("resizeCanvas", []int{2, 3}, args...)
+}
+
+func NoCanvas(args ...any) error {
+	return AnyFuncReturnErr("noCanvas", []int{0}, args...)
+}
+
+func CreateGraphics(args ...any) (js.Value, error) {
+	return AnyFuncReturnJsValue("CreateGraphics", []int{2, 3, 4}, args...)
+}
+
+func CreateFramebuffer(args ...any) (js.Value, error) {
+	return AnyFuncReturnJsValue("CreateFramebuffer", []int{0, 1}, args...)
+}
+
+func ClearDepth(args ...any) error {
+	return AnyFuncReturnErr("clearDepth", []int{0, 1}, args...)
+}
+
+func BlendMode(args ...any) error {
+	return AnyFuncReturnErr("blendMode", []int{1}, args...)
+}
+
+func SetAttributes(args ...any) error {
+	return AnyFuncReturnErr("setAttributes", []int{1, 2}, args...)
 }
