0915398cbec3f3f7fc6a7a3ce61bf0f53d83469f
Nosorozhek <nosorozhek239@gmail.com>
2024-04-26 10:29:10 +0300

    Refactored example

--- examples/main.go
+++ examples/main.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	. "p5js-wasm-go/color"
-	"p5js-wasm-go/events"
+	. "p5js-wasm-go/events"
 	. "p5js-wasm-go/p5js"
 	. "p5js-wasm-go/rendering"
-	_ "p5js-wasm-go/rendering"
 	. "p5js-wasm-go/shape"
 	"syscall/js"
-	_ "syscall/js"
 )
 
 func main() {
-	r := 20.0
-	c := 0
-	vars := [][]any{{239, 30, 30}, {30, 239, 30}, {30, 30, 239}}
-	_ = vars
+	radius := 20.0
+	colorIndex := 0
+	colors := [][]any{{239, 30, 30}, {30, 239, 30}, {30, 30, 239}}
+
 	Setup(func() interface{} {
 		_, err := CreateCanvas(400, 400)
 		if err != nil {
@@ -29,36 +27,36 @@ func main() {
 	})
 
 	Draw(func() interface{} {
-		err := Ellipse(events.MouseX(), events.MouseY(), r, r)
+		err := Ellipse(MouseX(), MouseY(), radius, radius)
 		if err != nil {
 			return nil
 		}
 		return nil
 	})
 
-	events.MouseMoved(func(args ...js.Value) interface{} {
+	MouseMoved(func(args ...js.Value) interface{} {
 		_ = NoStroke()
-		_ = Fill(vars[c%len(vars)]...)
+		_ = Fill(colors[colorIndex%len(colors)]...)
 		return nil
 	})
 
-	events.MouseClicked(func(args ...js.Value) interface{} {
-		c = c + 1
+	MouseClicked(func(args ...js.Value) interface{} {
+		colorIndex = colorIndex + 1
 		return nil
 	})
 
-	events.MouseDragged(func(args ...js.Value) interface{} {
+	MouseDragged(func(args ...js.Value) interface{} {
 		_ = Stroke(255, 255, 255)
 		_ = Fill(255, 255, 255, 63)
-		_ = Ellipse(events.MouseX(), events.MouseY(), r, r)
+		_ = Ellipse(MouseX(), MouseY(), radius, radius)
 		return nil
 	})
 
-	events.MouseWheel(func(args ...js.Value) interface{} {
+	MouseWheel(func(args ...js.Value) interface{} {
 		if args[0].Get("delta").Int() > 0 {
-			r *= 1.1
+			radius *= 1.1
 		} else {
-			r /= 1.1
+			radius /= 1.1
 		}
 		return nil
 	})
